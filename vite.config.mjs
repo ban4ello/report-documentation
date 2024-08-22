0a7517608af5dbe96100daa32f9e2cc3e75852fd
ban4ello <ban4ellog@gmail.com>
2024-08-22 13:50:29 +0300

    [code cleaning]

--- vite.config.mjs
+++ vite.config.mjs
@@ -8,7 +8,6 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   base: process.env.NODE_ENV === 'production' ? '/report-documentation' : '/',
-  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
   optimizeDeps: {
     noDiscovery: true
   },
