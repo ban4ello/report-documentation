77f988f516a75abf340fe74b0657a520af135f36
ban4ello <ban4ellog@gmail.com>
2024-08-22 04:57:41 +0300

    deploy 4

--- vite.config.mjs
+++ vite.config.mjs
@@ -7,8 +7,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: process.env.NODE_ENV === 'production' ? '/report-documentation/' : '/',
-  // publicPath: process.env.NODE_ENV === 'production' ? '/report-documentation/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/report-documentation/' : '/',
   optimizeDeps: {
     noDiscovery: true
   },
